2428c8a785f233f56c5a11f02afdfbc0c2813157
Romesh McCullough <romesh.mccullough@gmail.com>
2018-06-04 16:09:14 -0400

    add xds cluster type as a replacement token

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ set -xeuo pipefail
 
 sed -i "s/_NODE_ID_/${NODE_ID}/" /etc/envoy.yaml
 sed -i "s/_NODE_CLUSTER_/${NODE_CLUSTER}/" /etc/envoy.yaml
+sed -i "s/_XDS_CLUSTER_TYPE_/${XDS_CLUSTER_TYPE}/" /etc/envoy.yaml
 sed -i "s/_XDS_HOST_/${XDS_HOST}/" /etc/envoy.yaml
 sed -i "s/_XDS_PORT_/${XDS_PORT}/" /etc/envoy.yaml
 
