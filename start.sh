58a982253e9e5e5cd84df5abb002c83dc1060325
Romesh McCullough <romesh.mccullough@gmail.com>
2018-06-10 14:25:16 -0400

    default drain time to 90s

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ sed -i "s/_XDS_CLUSTER_TYPE_/${XDS_CLUSTER_TYPE}/" /etc/envoy.yaml
 sed -i "s/_XDS_HOST_/${XDS_HOST}/" /etc/envoy.yaml
 sed -i "s/_XDS_PORT_/${XDS_PORT}/" /etc/envoy.yaml
 
-envoy -c /etc/envoy.yaml & # TODO: exit if envoy fails to start due to bad config
+envoy -c /etc/envoy.yaml --drain-time-s 90 & # TODO: exit if envoy fails to start due to bad config
 docker-entrypoint.sh postgres
